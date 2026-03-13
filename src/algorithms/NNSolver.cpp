0b7247b47ec42dc132d8b51b655d3e9903a6cab7
Jakub Firlej <jesoen@o2.pl>
2026-03-13 22:49:49 +0100

    schedule added

--- src/algorithms/NNSolver.cpp
+++ src/algorithms/NNSolver.cpp
@@ -3,3 +3,51 @@
 //
 
 #include "NNSolver.h"
+
+#include <iostream>
+
+NNSolver::NNSolver(): ans(nullptr), size(0), cost(-1) {
+}
+
+NNSolver::~NNSolver() {
+    delete[] ans;
+}
+
+void NNSolver::solve(Graph& graph) {
+    // TODO: NAJPIERW ZOBACZYC KSIAZKE
+    // Nowoczesna heurystyka
+    // str 87 Metody wyliczeniowe TSP
+    // brute-force iteracyjny + rekurencyjny
+    //
+    // str 117 zachłanny
+    // najbliższy sąsiad
+    //
+    // TODO: ZOBACZYC STRONY:
+    // https://openstax.org/books/contemporary-mathematics/pages/12-9-traveling-salesperson-problem
+    // https://swistak.codes/post/problem-komiwojazera/
+    // TODO: zobaczyc pdfy wyslane z1_z2
+    // TODO: WYMYSLEC JAK ROZWIAZAC ALGORYTM
+
+}
+
+void NNSolver::print() {
+    if (!ans) {
+        std::cout<<"No solution found."<<std::endl;
+        return;
+    }
+
+    std::cout<<"Best NEAREST NEIGHBOUR ALGORITHM path: ";
+    for (int i = 0; i<size; i++) {
+        std::cout << ans[i] << " ";
+    }
+
+    std::cout << "\n\tCost = "<<cost<<std::endl;
+}
+
+int* NNSolver::getAns() {
+    return ans;
+}
+
+int NNSolver::getCost() {
+    return cost;
+}
--- src/algorithms/NNSolver.h
+++ src/algorithms/NNSolver.h
@@ -4,11 +4,22 @@
 
 #ifndef NNSOLVER_H
 #define NNSOLVER_H
+#include "../include/ISolver.h"
 
 
+class NNSolver : public ISolver {
+public:
+    NNSolver();
+    ~NNSolver() override;
+    void solve(Graph& graph) override;
+    void print() override;
 
-class NNSolver {
+    int* getAns() override;
+    int getCost() override;
 
+private:
+    int* ans;
+    int size, cost;
 };
 
 
